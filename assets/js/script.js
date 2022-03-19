82e4582b74b04046ef09ceb8decfa1c344be8e0c
Gabriel Barbosa <gabrielbarbosaoliveira1999@gmail.com>
2022-03-19 15:12:37 -0300

    Projeto - Finalizados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 function mais(){
     //Selecionando o elemente(Input de quandtidade) atraves do ID
-    var atual = document.getElementById("quantidade").value;
+    let atual = document.getElementById("quantidade").value;
     //Realizando a soma
-    var novo = parseInt(atual) + 1;
+    let novo = parseInt(atual) + 1;
     //Adicionando o novo valor no Input
     document.getElementById("quantidade").value = novo;
 }
   
 function menos(){
     //Selecionando o elemente(Input de quandtidade) atraves do ID
-    var atual = document.getElementById("quantidade").value;
+    let atual = document.getElementById("quantidade").value;
     if(atual > 1) { //Verificamos se o valor atual e maior q 1, caso nao seja nao deixa zerar!
         //Realizando a subtracao
-        var novo = atual - 1;
+        let novo = atual - 1;
         //Adicionando o novo valor no Input
         document.getElementById("quantidade").value = novo;
     }
